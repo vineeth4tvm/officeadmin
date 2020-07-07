b7d25d4847a50d816dd8eeb8d4e84cb08f97b4b9
vineeth4tvm <vineeth4tvm@gmail.com>
2020-07-07 22:47:42 +0530

    eod

--- controllers/officeadmin/employeeController.js
+++ controllers/officeadmin/employeeController.js
@@ -252,18 +252,9 @@ exports.view = function(req, res){
             }
             else{
                 
-            data.forEach(part => {
-                var joiningdate = part.joiningdate.toString();
-                console.log('joiningdate : '+ joiningdate)
-                jyear = joiningdate.slice(0,4);
-                jmonth = joiningdate.slice(4,6);
-                jday = joiningdate.slice(6,8);
-                part['joiningdate'] = jday+' '+jmonth+' '+jyear;
-                console.log(jday+' '+jmonth+' '+jyear )
-            }); }
-
+            
+            }
             res.render('view_employees', {employee: data, message: message});
-
         }
     });
 
